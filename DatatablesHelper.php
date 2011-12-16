977cb132612fc58f3aca402b077d235724d8e460
Matthew Urtnowski <Murtnowski@gmail.com>
2011-12-16 15:41:54 -0800

    Added Open Source Initiative OSI - The BSD 3-Clause License

--- DatatablesHelper.php
+++ DatatablesHelper.php
@@ -1,4 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	/**
+	 * Open Source Initiative OSI - The BSD 3-Clause License
+	 *
+	 * Copyright (c) 2011, Matthew Urtnowski
+	 * All rights reserved.
+	 *
+	 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+	 * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+	 * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+	 * Neither the name of Matthew Urtnowski nor the names of any contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	 */
 	
 	/**
 	 * Library to change a MySQL query to include the Datatables POST information
@@ -6,12 +20,8 @@
 	 * This controller takes in a MySQL query, parses it to identify its parts then injects
 	 * changes to the HAVING clause and adds the ORDER BY and LIMIT clauses.
 	 *
-	 * @program         TradesoftUK
 	 * @author          Matthew Urtnowski
-	 * @date            December 2011
-	 * @dependancy		php-sql-parser
-	 * @referance       http://www.datatables.net/
-	 * @referance       http://code.google.com/p/php-sql-parser/
+	 *
 	 */
 	class DatatablesHelper 
 	{
